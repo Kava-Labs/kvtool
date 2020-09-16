fee9cfe74c06f0c9a698087cdfb8168090f2576b
rhuairahrighairigh <ruaridh.odonnell@gmail.com>
2020-09-16 15:18:16 +0100

    support many kava versions with gen-config command

--- cmd/testnet.go
+++ cmd/testnet.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"os"
@@ -15,6 +16,12 @@ import (
 	"gopkg.in/yaml.v3"
 )
 
+const (
+	kavaServiceName    = "kava"
+	binanceServiceName = "binance"
+	deputyServiceName  = "deputy"
+)
+
 var (
 	// ProjectDir is the root directory of the kvtool repo.
 	// It's set at build time using an -X flag. eg -ldflags "-X github.com/kava-labs/kvtool/cmd.ProjectDir=/home/user1/kvtool"
@@ -22,13 +29,38 @@ var (
 
 	configTemplatesDir        string = filepath.Join(ProjectDir, "config_templates")
 	defaultGeneratedConfigDir string = filepath.Join(ProjectDir, "full_configs", "generated")
+
+	supportedServices = []string{kavaServiceName, binanceServiceName, deputyServiceName}
 )
+
+/*
+localnet => start just kava v0.10?
+localnet --binance => start just binance
+localnet --kava kava/kava:latest --binance --deputy --oracle => starts all components
+
+kvtool testnet gen-config
+kvtool testnet up
+kvtool testnet down
+kvtool testnet defaults to config --kava v0.10, and up
+
+
+specifying master kava isn't easy - which config should be paired with it? v0.10 won't work right now. Could specify genesis file additionally.
+But doesn't handle other changes: kvd flags (--pruning, unsafe cors), kvcli data
+
+Only going to be two versions of kava at any one time
+
+Support features piece by piece, always the manual edit fallback.
+specify kava version - if folder not present error
+person working on new kava branch can create new kvtool branch and put updated genesis file in master template
+
+--kava --binance --deputy --kava.configTemplate=dm-update-querier --deputy.configTemplate=d8c3e51
+*/
 
 func TestnetCmd() *cobra.Command {
 
 	var generatedConfigDir string
 
-	cmd := &cobra.Command{
+	rootCmd := &cobra.Command{
 		Use:   "testnet",
 		Short: "Start a local kava testnet",
 		Args:  cobra.NoArgs,
@@ -40,7 +72,7 @@ func TestnetCmd() *cobra.Command {
 			}
 
 			// 2) generate a complete docker-compose config
-			if err := generateFullConfig(configTemplatesDir, generatedConfigDir); err != nil {
+			if err := generateDefaultConfig(configTemplatesDir, generatedConfigDir); err != nil {
 				return fmt.Errorf("could not generate config: %v", err)
 			}
 
@@ -53,10 +85,52 @@ func TestnetCmd() *cobra.Command {
 			return nil
 		},
 	}
+	rootCmd.PersistentFlags().StringVar(&generatedConfigDir, "generated-dir", defaultGeneratedConfigDir, "output directory for the generated config")
 
-	cmd.Flags().StringVar(&generatedConfigDir, "generated-dir", defaultGeneratedConfigDir, "output directory for the generated config")
+	var kavaConfigTemplate string
 
-	return cmd
+	genConfigCmd := &cobra.Command{
+		Use:       "gen-config [services...]",
+		Short:     "Generate a complete docker-compose configuration for a new testnet.",
+		ValidArgs: supportedServices,
+		Args:      Minimum1ValidArgs,
+		RunE: func(_ *cobra.Command, args []string) error {
+
+			// 1) clear out generated config folder
+			if err := os.RemoveAll(generatedConfigDir); err != nil {
+				return fmt.Errorf("could not clear old generated config: %v", err)
+			}
+
+			// 2) generate a complete docker-compose config
+			if stringSlice(args).contains(kavaServiceName) {
+				if err := generateKavaConfig(configTemplatesDir, kavaConfigTemplate, generatedConfigDir); err != nil {
+					return err
+				}
+			}
+			if stringSlice(args).contains(binanceServiceName) {
+				if err := generateBnbConfig(configTemplatesDir, generatedConfigDir); err != nil {
+					return err
+				}
+			}
+			if stringSlice(args).contains(deputyServiceName) {
+				if err := generateDeputyConfig(configTemplatesDir, generatedConfigDir); err != nil {
+					return err
+				}
+			}
+			return nil
+		},
+	}
+	genConfigCmd.Flags().StringVar(&kavaConfigTemplate, "kava.configTemplate", "v0.10", "the directory name of the template used to generating the kava config")
+
+	rootCmd.AddCommand(genConfigCmd)
+	return rootCmd
+}
+
+func Minimum1ValidArgs(cmd *cobra.Command, args []string) error {
+	if len(args) < 1 {
+		return errors.New("must specify at least one argument")
+	}
+	return cobra.OnlyValidArgs(cmd, args)
 }
 
 func replaceCurrentProcess(command ...string) error {
@@ -78,8 +152,8 @@ func replaceCurrentProcess(command ...string) error {
 	return nil
 }
 
-func generateFullConfig(configTemplatesDir, generatedConfigDir string) error {
-	if err := generateKavaConfig(configTemplatesDir, generatedConfigDir); err != nil {
+func generateDefaultConfig(configTemplatesDir, generatedConfigDir string) error {
+	if err := generateKavaConfig(configTemplatesDir, "v0.10", generatedConfigDir); err != nil {
 		return err
 	}
 	if err := generateBnbConfig(configTemplatesDir, generatedConfigDir); err != nil {
@@ -91,16 +165,16 @@ func generateFullConfig(configTemplatesDir, generatedConfigDir string) error {
 	return nil
 }
 
-func generateKavaConfig(configTemplatesDir, generatedConfigDir string) error {
+func generateKavaConfig(configTemplatesDir, kavaConfigTemplate, generatedConfigDir string) error {
 	// copy templates into generated config folder
-	err := copy.Copy(filepath.Join(configTemplatesDir, "kava/v0.10"), filepath.Join(generatedConfigDir, "kava"))
+	err := copy.Copy(filepath.Join(configTemplatesDir, "kava", kavaConfigTemplate), filepath.Join(generatedConfigDir, "kava"))
 	if err != nil {
 		return err
 	}
 
 	// put together final compose file
 	err = overwriteMergeYAML(
-		filepath.Join(configTemplatesDir, "kava/v0.10/docker-compose.yaml"),
+		filepath.Join(configTemplatesDir, "kava", kavaConfigTemplate, "docker-compose.yaml"),
 		filepath.Join(generatedConfigDir, "docker-compose.yaml"),
 	)
 	return err
@@ -184,4 +258,15 @@ func exportYAML(filename string, data *gabs.Container) error {
 		return err
 	}
 	return nil
+}
+
+type stringSlice []string
+
+func (strings stringSlice) contains(match string) bool {
+	for _, s := range strings {
+		if match == s {
+			return true
+		}
+	}
+	return false
 }
