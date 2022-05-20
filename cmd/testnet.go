86385fac25fc3a0599606b8beca8982726cab943
Robert Pirtle <Astropirtle@gmail.com>
2022-05-20 13:49:48 -0700

    support running testnet in detached mode (#112)
    using the `-d` or `--detach` flag with `testnet up` command will spin up
    the containers in the background.

--- cmd/testnet.go
+++ cmd/testnet.go
@@ -124,12 +124,17 @@ available services: %s
 	genConfigCmd.Flags().BoolVar(&ibcFlag, "ibc", false, "flag for if ibc is enabled")
 	rootCmd.AddCommand(genConfigCmd)
 
+	var runDetachedFlag bool
+
 	upCmd := &cobra.Command{
 		Use:   "up",
 		Short: "A convenience command that runs `docker-compose up` on the generated config.",
 		Args:  cobra.NoArgs,
 		RunE: func(_ *cobra.Command, _ []string) error {
 			cmd := []string{"docker-compose", "--file", filepath.Join(generatedConfigDir, "docker-compose.yaml"), "up"}
+			if runDetachedFlag {
+				cmd = append(cmd, "-d")
+			}
 			fmt.Println("running:", strings.Join(cmd, " "))
 			if err := replaceCurrentProcess(cmd...); err != nil {
 				return fmt.Errorf("could not run command: %v", err)
@@ -137,6 +142,7 @@ available services: %s
 			return nil
 		},
 	}
+	upCmd.Flags().BoolVarP(&runDetachedFlag, "detach", "d", false, "Detached mode: Run containers in the background.")
 	rootCmd.AddCommand(upCmd)
 
 	downCmd := &cobra.Command{
