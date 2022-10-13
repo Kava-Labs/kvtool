c16c15709a34be051e0115347a41a480843ece47
Robert Pirtle <astropirtle@gmail.com>
2022-10-13 10:25:49 -0700

    support changing power distribution

--- contrib/update-genesis-validators/cmd/update_genesis_validators.go
+++ contrib/update-genesis-validators/cmd/update_genesis_validators.go
@@ -4,7 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"log"
+	"math/big"
+	"os"
 	"path/filepath"
 	"sort"
 	"time"
@@ -24,31 +25,47 @@ import (
 	tmtypes "github.com/tendermint/tendermint/types"
 )
 
-var ugvKeysDir string
-var ugvKeyPrefix string
-var ugvOutFile string
-var ugvChainID string
+// flag options for the command
+var (
+	// chain id for the modified genesis file's network
+	ugvChainID string
+	// minimum %age of power held by the included validators
+	ugvMinimumPowerPercent float64
+	// path to directory containing all private validator keys to use for replacement
+	ugvKeysDir string
+	// the prefix of the private validator key files
+	ugvKeyPrefix string
+	// path & name of file to save updated genesis to
+	ugvOutFile string
+)
 
 var updateGenesisValidatorsCmd = &cobra.Command{
 	Use:   "update-genesis-validators",
 	Short: "Updates the validators of a non-zero height genesis.json",
-	Long: `Takes a non-zero height genesis.json and a directory of indexed priv_validator_keys
-and replaces the validators of the genesis file with ones corresponding to the keys`,
+	Long: `Takes a non-zero height genesis.json and a directory of indexed priv_validator_keys and replaces the validators of the genesis file with ones corresponding to the keys.
+
+By default, the new validators replace the old ones from highest power to lowest. Any other validators are left as is.`,
 	Args: cobra.MinimumNArgs(1),
 	RunE: updateGenesisValidators,
 }
 
 func Execute() {
 	if err := updateGenesisValidatorsCmd.Execute(); err != nil {
-		log.Fatal(err)
+		fmt.Println(err)
+		os.Exit(1)
 	}
 }
 
 func init() {
 	updateGenesisValidatorsCmd.Flags().StringVar(
 		&ugvChainID,
-		"chain-id", "updated-genesis.json",
+		"chain-id", "",
 		"Chain id of modified output genesis file.",
+	)
+	updateGenesisValidatorsCmd.Flags().Float64Var(
+		&ugvMinimumPowerPercent,
+		"min-power", 0,
+		"Optional, minimum percentage of total power given to replaced validators.",
 	)
 	updateGenesisValidatorsCmd.Flags().StringVarP(
 		&ugvKeysDir,
@@ -70,6 +87,10 @@ Validators are replaced by in order of highest voting power.`,
 }
 
 func updateGenesisValidators(cmd *cobra.Command, args []string) error {
+	if ugvMinimumPowerPercent >= 1 || ugvMinimumPowerPercent < 0 {
+		return fmt.Errorf("minimum power is a percent. out of range: 0 <= power < 1")
+	}
+
 	filename := args[0]
 	doc, err := loadGenesisFile(filename)
 	if err != nil {
@@ -87,15 +108,15 @@ func updateGenesisValidators(cmd *cobra.Command, args []string) error {
 		return fmt.Errorf("failed to load validator keys: %s", err)
 	}
 
-	log.Printf("found %d validator keys\n", len(valKeys))
+	fmt.Printf("found %d validator keys\n", len(valKeys))
 
 	// update chain id or warn
 	if ugvChainID == "" || ugvChainID == doc.ChainID {
-		log.Printf("%s%s\n",
+		fmt.Printf("%s%s\n",
 			"WARNING: the output will have the same chain id.",
 			" This can put the new chain at risk of replay attacks.",
 		)
-		log.Println("Consider changing the chain id with the --chain-id flag.")
+		fmt.Println("Consider changing the chain id with the --chain-id flag.")
 	} else {
 		doc.ChainID = ugvChainID
 	}
@@ -110,7 +131,7 @@ func updateGenesisValidators(cmd *cobra.Command, args []string) error {
 		return fmt.Errorf("failed to save output file: %s", err)
 	}
 
-	log.Printf("successfully updated validators. file saved to %s\n", ugvOutFile)
+	fmt.Printf("successfully updated validators. file saved to %s\n", ugvOutFile)
 
 	return nil
 }
@@ -142,7 +163,7 @@ func loadValidatorKeys(dir string, prefix string) ([]pvtypes.FilePVKey, error) {
 	}
 
 	if len(valKeys) == 0 {
-		log.Printf("WARN: no json files with prefix %s found in directory %s. continuing anyway.\n", ugvKeyPrefix, ugvKeysDir)
+		fmt.Printf("WARN: no json files with prefix %s found in directory %s. continuing anyway.\n", ugvKeyPrefix, ugvKeysDir)
 		return valKeys, nil
 	}
 
@@ -155,8 +176,8 @@ func UpdateGenesisFileWithNewValidators(
 ) error {
 	// warn that we aren't using all val keys
 	if len(doc.Validators) < len(valKeys) {
-		log.Println("warning: more validator keys provided than original validators.")
-		log.Printf(
+		fmt.Println("warning: more validator keys provided than original validators.")
+		fmt.Printf(
 			"  continuing with replacement but will only inject %d new validators\n",
 			len(doc.Validators),
 		)
@@ -173,22 +194,33 @@ func UpdateGenesisFileWithNewValidators(
 		return doc.Validators[i].Power > doc.Validators[j].Power
 	})
 
-	// build map of old valcons -> new valcons
-	replacements := make(map[string]pvtypes.FilePVKey, numReplace)
+	// preprocess and replace `.validators`
+	var (
+		// total initial power of replaced validators
+		initialValPower = sdk.NewInt(0)
+		// a map of original valcons address -> new valcons private key
+		replacements = make(map[string]pvtypes.FilePVKey, numReplace)
+	)
 	for i := 0; i < numReplace; i++ {
+		// build old -> new valcons mapping
 		orig := sdk.ConsAddress(doc.Validators[i].Address)
 		replacements[orig.String()] = valKeys[i]
+
+		fmt.Printf("replacing %s -> %s\n", orig, sdk.ConsAddress(valKeys[i].Address))
 
 		// replace pub key in `.validators` array
 		doc.Validators[i].PubKey = valKeys[i].PubKey
 		doc.Validators[i].Address = valKeys[i].Address
+
+		// add validator power to total
+		initialValPower = initialValPower.AddRaw(doc.Validators[i].Power)
 	}
 
 	// update `genesis_time`
 	// we need to do this to ensure there's no consensus error
 	// see https://github.com/tendermint/tendermint/issues/8773
 	doc.GenesisTime = time.Now()
-	log.Println("updated genesis_time to now")
+	fmt.Println("updated genesis_time to now")
 
 	// unmarshal app state so we can unmarshal state of relevant modules
 	encodingConfig := app.MakeEncodingConfig()
@@ -208,9 +240,23 @@ func UpdateGenesisFileWithNewValidators(
 		return fmt.Errorf("failed to unmarshal app_state.staking: %s", err)
 	}
 
+	// determine current total power percentage
+	totalPowerDelta := calcPowerDelta(
+		stakingState.LastTotalPower,
+		initialValPower,
+		ugvMinimumPowerPercent,
+	)
+	powerDelta := sdk.NewInt(0)
+	if !totalPowerDelta.Equal(powerDelta) {
+		powerDelta = totalPowerDelta.QuoRaw(int64(numReplace))
+	}
+
+	fmt.Printf("total power delta: %s; per validator: %s\n", totalPowerDelta.String(), powerDelta)
+
 	// replace the `consensus_pubkey.value` of `.app_state.staking.validators`
+	// flag the valoper addresses we'll update power of
+	valoperAddresses := make(map[string]struct{}, numReplace)
 	for i, val := range stakingState.Validators {
-		// orig := sdk.ValAddress(val.ConsensusPubkey.Value)
 		var orig cryptotypes.PubKey
 		err = codec.UnpackAny(val.ConsensusPubkey, &orig)
 		if err != nil {
@@ -227,8 +273,27 @@ func UpdateGenesisFileWithNewValidators(
 			}
 			// set new pubkey!
 			stakingState.Validators[i].ConsensusPubkey = pubKey
+
+			// flag the valoper address for power adjustments
+			valoperAddresses[val.OperatorAddress] = struct{}{}
 		}
 	}
+
+	// update `last_validator_powers` to increase validator power
+	for i, valPow := range stakingState.LastValidatorPowers {
+		if _, replaced := valoperAddresses[valPow.Address]; replaced {
+			fmt.Printf("updating power for %s: %d -> ", valPow.Address, valPow.Power)
+			stakingState.LastValidatorPowers[i].Power += powerDelta.Int64()
+			fmt.Println(stakingState.LastValidatorPowers[i].Power)
+		}
+	}
+
+	// update `last_total_power` to new total
+	fmt.Printf("increasing total power: %s -> ", stakingState.LastTotalPower.String())
+	stakingState.LastTotalPower = stakingState.LastTotalPower.Add(
+		powerDelta.MulRaw(int64(numReplace)),
+	)
+	fmt.Println(stakingState.LastTotalPower.String())
 
 	// remarshal updated state
 	appState[stakingtypes.ModuleName], err = codec.MarshalJSON(&stakingState)
@@ -284,7 +349,9 @@ func UpdateGenesisFileWithNewValidators(
 	// update valcons address in previous `.app_state.distribution.previous_proposer`
 	replacement, found := replacements[distributionState.PreviousProposer]
 	if found {
+		fmt.Printf("updating previous proposer: %s -> ", distributionState.PreviousProposer)
 		distributionState.PreviousProposer = sdk.ConsAddress(replacement.Address.Bytes()).String()
+		fmt.Println(distributionState.PreviousProposer)
 	}
 
 	// remarshal updated state
@@ -296,6 +363,12 @@ func UpdateGenesisFileWithNewValidators(
 	//----------------------
 	// FINALIZE APP STATE
 	//----------------------
+	// update the power of `validators` (required staking state read first)
+	// doc.validators are still sorted so its safe to iterate by number
+	for i := 0; i < numReplace; i++ {
+		doc.Validators[i].Power += powerDelta.Int64()
+	}
+
 	// finalize the app_state updates!
 	doc.AppState, err = json.Marshal(appState)
 	if err != nil {
@@ -303,4 +376,42 @@ func UpdateGenesisFileWithNewValidators(
 	}
 
 	return nil
+}
+
+// calcPowerDelta calculates the necessary total power increase that, when given to the replaced
+// validators, adjusts the total power such that the replaced validators control at least the
+// desired percentage
+func calcPowerDelta(
+	initialTotalPower sdk.Int,
+	initialValPower sdk.Int,
+	desiredPercent float64,
+) sdk.Int {
+	iTotalPower := new(big.Float).SetInt(initialTotalPower.BigInt())
+	iValPower := new(big.Float).SetInt(initialValPower.BigInt())
+	initialPercent := new(big.Float).Quo(iValPower, iTotalPower)
+	fmt.Printf("initial power = %s / %s = %s\n", initialValPower.String(), initialTotalPower.String(), initialPercent.String())
+
+	percentAfter := big.NewFloat(desiredPercent)
+	// if we already have enough power, no change is necessary
+	if initialPercent.Cmp(percentAfter) >= 0 {
+		return sdk.NewInt(0)
+	}
+
+	// a = (P + Δ) / (T + Δ) => Δ = (a*T - P) / (1 - a)
+	// Δ - total change in power given to replaced validators
+	// a - desired percentage of total power
+	// P - initial power of replaced validators
+	// T - initial total power of all validators
+	num := new(big.Float).Sub(
+		new(big.Float).Mul(percentAfter, iTotalPower),
+		iValPower,
+	)
+	den := new(big.Float).Sub(big.NewFloat(1), percentAfter)
+	delta := new(big.Float).Quo(num, den)
+
+	// convert the delta to a big int
+	roundedDelta := new(big.Int)
+	delta.Int(roundedDelta)
+	// add 1 to ensure any rounding is in our validators' favor
+	return sdk.NewIntFromBigInt(roundedDelta).AddRaw(1)
 }
