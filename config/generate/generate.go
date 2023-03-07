2e58c3c89ee386ac61feed06b3777ba68f073704
Robert Pirtle <astropirtle@gmail.com>
2023-03-07 12:08:40 -0800

    ensure all relayer files have read/write perms

--- config/generate/generate.go
+++ config/generate/generate.go
@@ -119,11 +119,10 @@ func AddHermesRelayerToNetwork(generatedConfigDir string) error {
 }
 
 func GenerateGoRelayerConfig(generatedConfigDir string) error {
-	err := copy.Copy(filepath.Join(ConfigTemplatesDir, "relayer"), filepath.Join(generatedConfigDir, "relayer"))
-	if err != nil {
-		return err
-	}
-	// config.yaml must be writable
-	err = os.Chmod(filepath.Join(generatedConfigDir, "relayer", "config", "config.yaml"), 0666)
+	err := copy.Copy(
+		filepath.Join(ConfigTemplatesDir, "relayer"),
+		filepath.Join(generatedConfigDir, "relayer"),
+		copy.Options{AddPermission: 0666},
+	)
 	return err
 }
