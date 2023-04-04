625bcbe9205c5c1d934f0d07101c003c3447af0d
Robert Pirtle <Astropirtle@gmail.com>
2023-04-04 14:01:53 -0700

    update json-rpc ip to 0.0.0.0  & set max_gas (#151)
    * update json-rpc ip from localhost to 0.0.0.0
    
    * set maximum gas allowed per block

--- config/generate/genesis/generate-kava-genesis.sh
+++ config/generate/genesis/generate-kava-genesis.sh
@@ -90,6 +90,10 @@ sed -i '' 's/minimum-gas-prices = "0ukava"/minimum-gas-prices = "0.001ukava;1000
 # Disable pruning
 sed -i '' 's/pruning = "default"/pruning = "nothing"/g' $DATA/config/app.toml
 
+# Set EVM JSON-RPC starting IP addresses
+sed -i '' 's/address = "127.0.0.1:8545"/address = "0.0.0.0:8545"/g' $DATA/config/app.toml
+sed -i '' 's/ws-address = "127.0.0.1:8546"/ws-address = "0.0.0.0:8546"/g' $DATA/config/app.toml
+
 #######################
 ##### CLIENT.TOML #####
 #######################
@@ -102,7 +106,6 @@ sed -i '' 's/chain-id = ""/chain-id = "'"$chainID"'"/g' $DATA/config/client.toml
 # lower default commit timeout
 sed -i '' 's/timeout_commit = "5s"/timeout_commit = "1s"/g' $DATA/config/config.toml
 
-
 #########################
 ##### CONFIGURATION #####
 #########################
@@ -111,6 +114,12 @@ $BINARY config keyring-backend test
 
 # set broadcast-mode to block
 $BINARY config broadcast-mode block
+
+############################
+##### CONSENSUS PARAMS #####
+############################
+# set maximum gas allowed per block
+jq '.consensus_params.block.max_gas = "2000000"' $DATA/config/genesis.json | sponge $DATA/config/genesis.json
 
 ###########################
 ##### SETUP ADDRESSES #####
