35829ef54ed1738268a2e0f623b3367d2d6141ca
Robert Pirtle <astropirtle@gmail.com>
2023-06-12 10:49:48 -0700

    echo copy-gen progress

--- contrib/valval/copy-gen.sh
+++ contrib/valval/copy-gen.sh
@@ -5,5 +5,6 @@ num_validators=$(tr -d '[:space:]' <NUM_VALIDATORS)
 for ((i = 1; i <= num_validators; i++)); do
   home=kava-$i
 
+  echo copying genesis to "$home"
   cp updated-genesis.json $home/config/genesis.json
 done
