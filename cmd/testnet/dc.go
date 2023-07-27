cebb9b265e61fdea3b8b5959a5a6c55d465cd598
Robert Pirtle <astropirtle@gmail.com>
2023-07-27 12:01:15 -0700

    add ibc transfer example to kvtool testnet dc

--- cmd/testnet/dc.go
+++ cmd/testnet/dc.go
@@ -19,7 +19,20 @@ $ kvtool testnet dc exec kavanode /bin/bash
 
 Run some kava cli commands:
 $ kvtool testnet dc exec kavanode kava keys add magic-account
-$ kvtool testnet dc exec kavanode -- kava tx bank send whale <address> 1000000000ukava --gas-prices 1000ukava -y`,
+$ kvtool testnet dc exec kavanode -- kava tx bank send whale <address> 1000000000ukava --gas-prices 1000ukava -y
+
+If the testnet was started with --ibc, you can IBC transfer coins ot Kava from the ibc chain.
+Transfer 1 ATOM from the ibc chain to random account on Kava:
+$ kvtool t dc exec ibcnode -- kava tx ibc-transfer transfer \
+  transfer channel-0 kava1dwktgf8jcuusc885myae3hjk63jrc6tsz69muu 1000000uatom \
+	--from whale2 --gas auto --gas-adjustment 1.2 --gas-prices 0.01uatom
+
+Checking the balance on the Kava Chain:
+$ kvtool t dc exec kavanode -- kava q bank balances kava1dwktgf8jcuusc885myae3hjk63jrc6tsz69muu
+balances:
+- amount: "1000000"
+  denom: ibc/27394FB092D2ECCD56123C74F36E4C1F926001CEADA9CA97EA622B25F41E5EB2
+`,
 		Args: cobra.ArbitraryArgs,
 		RunE: func(_ *cobra.Command, args []string) error {
 			cmd := []string{"docker-compose", "--file", generatedPath("docker-compose.yaml")}
