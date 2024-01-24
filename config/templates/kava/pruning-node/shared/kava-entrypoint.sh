6d5de5f54f3933fc897fb187bcf15c833ab4f4d9
Robert Pirtle <astropirtle@gmail.com>
2024-01-24 12:04:14 -0800

    fix pruning node settings & entrypoint retries

--- config/templates/kava/pruning-node/shared/kava-entrypoint.sh
+++ config/templates/kava/pruning-node/shared/kava-entrypoint.sh
@@ -26,7 +26,10 @@ MAX_RETRIES=10
 DELAY_SECONDS=2
 
 for ((i = 0; i < MAX_RETRIES; i++)); do
-  VALIDATOR_NODE_ID="$(kava --node http://kavanode:26657 status | jq -r .node_info.id)" && break
+  VALIDATOR_NODE_ID=$(kava --node http://kavanode:26657 status 2>/dev/null | jq -r .node_info.id)
+  if [ -n "$VALIDATOR_NODE_ID" ]; then
+    break
+  fi
   sleep $DELAY_SECONDS
 done
 
